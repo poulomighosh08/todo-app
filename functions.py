c6b9aea0ae70cbe922e9a372b0ed1cccd1db2d26
poulomighosh08 <ghoshpoulomi1308@gmail.com>
2023-08-08 16:28:10 +0530

    Implemented edit button

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-def get_todos(filePath="files/todos.txt"):
+def get_todos(filePath="todos.txt"):
     """ Reads a text file and
     returns list of to-do items."""
     with open(filePath, 'r') as file_todo:
         todo_items = file_todo.readlines()
     return todo_items
 
-def write_todos(content, filePath="files/todos.txt"):
+def write_todos(content, filePath="todos.txt"):
     """ Write to-do items in a text file """
     with open(filePath, 'w') as file:
         file.writelines(content)
--- gui.py
+++ gui.py
@@ -2,10 +2,38 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", size=[45,10], enable_events=True)
 add_button = sg.Button("Add")
-window = sg.Window("My to-do app", layout=[[label], [input_box, add_button]])
-window.read()
+edit_button = sg.Button("Edit")
+window = sg.Window("My to-do app",
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=("Helvetica", 20))
+
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value["todo"] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todos = functions.get_todos()
+            new_todo = value["todo"] + '\n'
+            index = todos.index(value['todos'][0])
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(todos)
+        case "todos":
+           todo_to_edit = value['todos'][0]
+           window['todo'].update(values=todo_to_edit)
+
+        case sg.WIN_CLOSED:
+            break
 window.close()
 
 
